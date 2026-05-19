af96780b58970ab57d6641eec4090817723ad87e
varun <varunbc3@gmail.com>
2026-05-19 16:13:06 +0530

    latest commit

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
 #Provider of cloud
 provider "aws" {
-    region = ap-south-1
+  region = ap-south-1
 }
 
 # create a VPC with cidr block
 resource "aws_vpc" "terraform_vpc" {
-    cidr_block = "10.0.0.0/16"
-    
-    tags = {
-      Name = "tvpc"
-    }
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tvpc"
+  }
 }
 
 #Create a internetgateway and attch to above vpc id
 resource "aws_internet_gateway" "terraform_igw" {
-    
-    vpc_id = aws_vpc.terraform_vpc.id
 
-    tags = {
-      Name = "tigw"
-    }
-  
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  tags = {
+    Name = "tigw"
+  }
+
 }
 
 #Create a Public Subnet 1
 
 resource "aws_subnet" "terraform_s1" {
-  vpc_id     = aws_vpc.terraform_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.terraform_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 
   tags = {
@@ -38,8 +38,8 @@ resource "aws_subnet" "terraform_s1" {
 #Create a Public Subnet 2
 
 resource "aws_subnet" "terraform_s2" {
-  vpc_id     = aws_vpc.terraform_vpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.terraform_vpc.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
 
   tags = {
@@ -50,8 +50,8 @@ resource "aws_subnet" "terraform_s2" {
 #Create a Public Route Table for above vpc id and add routes
 
 resource "aws_route_table" "terraform_RT" {
-    vpc_id = aws_vpc.terraform_vpc.id
-    
+  vpc_id = aws_vpc.terraform_vpc.id
+
 
   route {
     cidr_block = "10.0.0.0/16"
@@ -59,7 +59,7 @@ resource "aws_route_table" "terraform_RT" {
   }
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block             = "0.0.0.0/0"
     egress_only_gateway_id = aws_internet_gateway.terraform_igw.id
   }
 
@@ -83,28 +83,28 @@ resource "aws_route_table_association" "terraform_associate2" {
 # Create a EC2 instance in Public subnet 1
 
 resource "aws_instance" "ec2-1" {
-    ami = "ami-09ed39e30153c3bf9"
-    instance_type = "t3.small"
-    iam_instance_profile = "ec2-ssm"
-    subnet_id = aws_subnet.terraform_s1.id
+  ami                  = "ami-09ed39e30153c3bf9"
+  instance_type        = "t3.small"
+  iam_instance_profile = "ec2-ssm"
+  subnet_id            = aws_subnet.terraform_s1.id
 
-    tags = {
-      Name = "firstinstance"
-    }
-  
+  tags = {
+    Name = "firstinstance"
+  }
+
 }
 
 # Create a EC2 instance in Public subnet 1
 
 resource "aws_instance" "ec2-2" {
-    ami = "ami-09ed39e30153c3bf9"
-    instance_type = "t3.small"
-    iam_instance_profile = "ec2-ssm"
-    subnet_id = aws_subnet.terraform_s2.id
-  
-      tags = {
-      Name = "secondinstance"
-    }
+  ami                  = "ami-09ed39e30153c3bf9"
+  instance_type        = "t3.small"
+  iam_instance_profile = "ec2-ssm"
+  subnet_id            = aws_subnet.terraform_s2.id
+
+  tags = {
+    Name = "secondinstance"
+  }
 }
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "vpcname" {
-    value = aws_vpc.terraform_vpc.id
+  value = aws_vpc.terraform_vpc.id
 }
 
 output "subnet1" {
-    value = aws_subnet.terraform_s1.cidr_block    
+  value = aws_subnet.terraform_s1.cidr_block
 }
 
 output "subnet2" {
-    value = aws_subnet.terraform_s2.cidr_block
-  
+  value = aws_subnet.terraform_s2.cidr_block
+
 }
 
 output "internetgatway" {
-    value = aws_internet_gateway.terraform_igw.id
+  value = aws_internet_gateway.terraform_igw.id
 }
 
 output "RouteTable" {
-    value = aws_route_table.terraform_RT.id
-  
+  value = aws_route_table.terraform_RT.id
+
 }
 
-output "RT Associates1" {
-    value = aws_route_table_association.terraform_associate1.subnet_id.id
-  
+output "RT-Associates1" {
+  value = aws_route_table_association.terraform_associate1.subnet_id.id
+
 }
 
-output "RT Associate2" {
-    value = aws_route_table_association.terraform_associate2.subnet_id.id
-  
+output "RT-Associate2" {
+  value = aws_route_table_association.terraform_associate2.subnet_id.id
+
 }
