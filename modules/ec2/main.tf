ad31b85ff579959b89f8f59cede2628e80e52e6c
varun <varunbc3@gmail.com>
2026-05-20 20:08:00 +0530

    update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -9,19 +9,23 @@ resource "aws_security_group" "EFSSG" {
  }
 
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.EFSSG.id
-  cidr_ipv4         = "0.0.0.0/0"
+resource "aws_security_group_rule" "ingressrule" {
+  type              = "ingress"
   from_port         = 80
-  ip_protocol       = "http"
   to_port           = 80
+  protocol          = "http"
+  cidr_blocks       = "0.0.0.0/0"
+  security_group_id = aws_security_group.EFSSG.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_security_group_rule" "egressrule" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
   security_group_id = aws_security_group.EFSSG.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
 
 resource "aws_instance" "ec2" {
   ami                  = var.mamiid
